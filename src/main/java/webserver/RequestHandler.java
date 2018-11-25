5d50c33e7a85fa7a94d64b0690853eb25f9f72d9
dev-dongwon <dev.dongwonkim@gmail.com>
2018-11-25 12:27:31 +0900

    index.html 응답하기 요구사항 구현
    - bufferedReader로 request 요청 읽어오기
    - 요청 주소인 index.html 가져오기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,11 +25,38 @@ public class RequestHandler extends Thread {
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
+        
+        // 자원을 다 사용하면 반드시 clsoe를 사용해줘야 하지만
+        // try 절을 활용하면 clsoe를 쓰지 않아도 해당 자원의 사용이 모두 끝나면 clsoe를 해준다
+        // closeable 인터페이스를 implement 하고 있기 때문
+        
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	
+        	// 클라이언트가 보내는 header를 line 단위로 읽기 위해 bufferedReader 를 사용
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        	String line = br.readLine();
+        	
+        	// null 값 무한 루프 방지, 빠른 종료 (test 코드로 인지)
+        	if (line == null) {
+        		return;
+        	}
+        	
+        	// index.html이 요청이 있는 라인을 가져온다
+        	String[] splited = line.split(" ");
+        	String path = splited[1];
+        	log.debug("request path : {}", path);
+        	
+        	/*
+        	 * test 코드
+        	 * 
+        	 * while(!"".equals(line)) {
+        		log.debug("header : {}", line);
+        		line = br.readLine();
+        		}
+        	*/
+        	
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
